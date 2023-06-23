7aaddaa452b6fd7c5093e3b0585899bd7b11cefa
unknown <Shubham.Pandey@moreyeahs.in>
2023-06-23 12:35:52 +0530

    design-improved

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import LandingPage from './pages/LandingPage/LandingPage';
 import Footer from './components/Footer';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [otp,setOtp]= useState("")
+  useEffect(()=>{
+    const ac = new AbortController();
+    navigator.credentials
+    .get({
+      otp: { transport: ["sms"] },
+      signal: ac.signal
+    })
+    .then((otp) => {
+      setOtp(otp)
+      console.log("h=>>>>>>",otp)
+      ac.abort();
+    })
+    .catch((err) => {
+      ac.abort();
+      console.log(err);
+    });
+  })
   return (
     <div className="App">
      <Header/>
