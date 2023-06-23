931daf7664161421db172a27040981d98653df86
unknown <Shubham.Pandey@moreyeahs.in>
2023-06-23 14:52:37 +0530

    design-improved

--- src/App.js
+++ src/App.js
@@ -5,25 +5,7 @@ import Footer from './components/Footer';
 import { useEffect, useState } from 'react';
 
 function App() {
-  const [otps,setOtp]= useState("")
-  useEffect(()=>{
-    const ac = new AbortController();
-    navigator.credentials
-    .get({
-      otp: { transport: ["otp"] },
-      signal: ac.signal
-    })
-    .then((otp) => {
-      setOtp(otp)
-      console.log("h=>>>>>>",otp)
-      ac.abort();
-    })
-    .catch((err) => {
-      ac.abort();
-      alert(err)
-      console.log(err);
-    });
-  })
+
   return (
     <div className="App">
      <Header/>
